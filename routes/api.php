e4da8de92fcdc5fe97635fe47f0d890f4c06305e
github <a.z.tvgg@icloud.com>
2021-06-11 18:46:16 +0300

    сделал регистрацию и авторизацию пользователей по токену

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::post('register', 'App\Http\Controllers\API\RegisteredUserController@register');
+
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
